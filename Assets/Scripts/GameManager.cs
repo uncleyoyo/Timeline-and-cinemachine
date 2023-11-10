86942e5d244c05356c61ff2550d22271263e7c52
Paul Kantoski <unkleyoyo@yahoo.com>
2023-11-09 20:19:00 -0500

    Added UI and a Game Manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] GameObject showInstructions;
+    bool instructionsActive;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ShowInstructions());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Quit();
+        }
+        if (Input.GetKeyDown(KeyCode.Q) && !instructionsActive)
+        {
+            StartCoroutine(ShowInstructions());
+            instructionsActive = true;
+        }
+
+    }
+    IEnumerator ShowInstructions()
+    {
+        showInstructions.SetActive(true);
+        yield return new WaitForSeconds(5);
+        showInstructions.SetActive(false);
+        instructionsActive = false;
+    }
+
+    void Quit()
+    {
+        Application.Quit();
+    }
+}
