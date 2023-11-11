6a25ebdb0431536d79b130abfbe367503a34d7f7
Paul Kantoski <unkleyoyo@yahoo.com>
2023-11-11 17:44:39 -0500

    Added controls to the ship

---
+++ Assets/Scripts/ShipControls.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipControls : MonoBehaviour
+{
+    [SerializeField]  float rotSpeed;
+    [SerializeField]  float moveSpeed;
+    [SerializeField]  float currentSpeed;
+    float vertical;
+    float horizontal;
+    [SerializeField]  float maxRotate;
+    [SerializeField]  GameObject shipModel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSpeed = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ShipMovement();
+    }
+
+    private void ShipMovement()
+    {
+        vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            currentSpeed++;
+            if (currentSpeed > 4)
+            {
+                currentSpeed = 4;
+            }
+        }//increase speed
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            currentSpeed--;
+            if (currentSpeed < 1)
+            {
+                currentSpeed = 1;
+            }
+        }//decrease speed
+
+        Vector3 rotateH = new Vector3(0, horizontal, 0);
+        transform.Rotate(rotateH * rotSpeed * Time.deltaTime);
+
+        Vector3 rotateV = new Vector3(vertical, 0, 0);
+        transform.Rotate(rotateV * rotSpeed * Time.deltaTime);
+
+        transform.Rotate(new Vector3(0, 0, -horizontal * 0.2f), Space.Self);
+
+        transform.position += transform.forward * currentSpeed * Time.deltaTime;
+    }
+}
