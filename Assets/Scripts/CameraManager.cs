fbdf95997c83ecbc761f3b4e132d994608e218ed
Paul Kantoski <unkleyoyo@yahoo.com>
2023-11-15 19:15:21 -0500

    played with the Camera Manager to fix the rotation issue

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-
-    [SerializeField] Transform playerShip;
+    [SerializeField] Transform shipTransform;
+    [SerializeField] float followSpeed;
+   
     // Start is called before the first frame update
     void Start()
     {
-        
+      
+        if (shipTransform == null)
+            Debug.LogError("ship transform not found");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (shipTransform != null)
+            MoveCamera(shipTransform);
+    }
+
+    void MoveCamera(Transform parentTransform)
+    {
+        Vector3 targetPostion = shipTransform.position;
+
+        parentTransform.position = Vector3.Lerp(parentTransform.position, targetPostion, followSpeed* Time.deltaTime);
     }
 }
