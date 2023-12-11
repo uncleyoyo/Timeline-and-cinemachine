d5aab8ee4403d4da7ebcb38ef7f5114e620f0d11
Paul Kantoski <unkleyoyo@yahoo.com>
2023-12-10 20:57:00 -0500

    Finished rough state of sim, needs little polish

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -37,5 +38,10 @@ public class GameManager : MonoBehaviour
     void Quit()
     {
         Application.Quit();
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
     }
 }
